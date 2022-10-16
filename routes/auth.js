94114940e182389777ff281f749bdcdbd2825626
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-10-16 04:27:49 -0400

    users added to server side

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,45 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt")
+
 
 //Register
-router.get("/register", async (req, res) => {
-    const user = await new User({
-        username: "Bocar",
-        email: "boc@yahoo.com",
-        password: "12345678"
-    })
+router.post("/register", async (req, res) => {
+   
+   try {
+    //Generate new secure password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+     
+    //Generate new user
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+      });
 
-   user.save();
-
-   res.send("Its working..")
+    //Save user and send response
+    const user = await newUser.save();
+     res.status(200).json(user)
+   } catch (error) {
+       console.log(error)
+   }
 });
 
+//LOGIN
+router.post("/login", async (req, res) => {
+   try {
+    const user = await User.findOne({email: req.body.email});
+    !user && res.status(404).json("user not found");
 
-module.exports = router
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("Incorrect password!")
+
+    res.status(200).json(user)
+
+   } catch (error) {
+      console.log(error)
+   }
+});
+
+module.exports = router;
