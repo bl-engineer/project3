2fd9206994ad51f7461a607fec95de96171eb2ff
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-10-16 06:36:10 -0400

    server side api's

--- index.js
+++ index.js
@@ -2,29 +2,31 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
 const helmet = require("helmet");
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
 mongoose.connect(
-    process.env.MONGO_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log("Connected to MongoDB");
-    }
-  );
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
 
-//Middleware
+//middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(3001, () => {
-    console.log("Backend server running on port: 3001!")
-})
+  console.log("Backend server is running!");
+});
